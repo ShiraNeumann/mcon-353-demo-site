ffb4522685d3e41fc66e0ba423682056de14f46a
Shira Neumann <sneumann3@student.touro.edu>
2023-02-09 19:50:41 -0500

    Made todo List and separate home file

---
+++ src/components/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Todo = () => {
+    return (
+        <>
+            <h1>To dos:</h1>
+            <p>Cook for Shabbos</p>
+            <p>Finish homework</p>
+            <p>Hang out with friends</p>
+        </>
+    );
+}
