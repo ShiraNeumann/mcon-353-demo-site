2401c922a2ddbce2d75a4ec46101d1d1f56da999
Shira Neumann <sneumann3@student.touro.edu>
2023-02-16 19:36:10 -0500

    Routing and added header

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import computer from "./images/computer1.jpg";
-import javaLogo from "./images/Java.png";
-import html from "./images/html.png";
-import css from "./images/css.png";
-import sql from "./images/sql.png";
-import python from "./images/python.png";
-import csharp from "./images/c#.png";
 import "./App.css";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Grid } from "@mui/material";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { Avatar, Button } from "@mui/material";
-import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+import { Header } from "./components/header/header";
 import { Home } from "./components/home/home";
 import { Todo } from "./components/todo/todo";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  //return <Home />;
-  return <Todo />;
+  return (
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import { CheckBox } from "@mui/icons-material";
+import React, { useState } from "react";
 
 export const Todo = () => {
-    return (
-        <>
-            <h1>To dos:</h1>
-            <p>Cook for Shabbos</p>
-            <p>Finish homework</p>
-            <p>Hang out with friends</p>
-        </>
-    );
-}
+  const [input, setInput] = useState(""); //use hook for keeping track of state
+  const [todos, setTodos] = useState([]);
+
+  const onInput = (event) => {
+    console.log(event.target.value);
+    setInput(event.target.value);
+  };
+
+  const addToDo = () => {
+    //use the setTodos method to create new array instead of mutating the data, so that the page knows to rerender
+    setTodos([...todos, input]); //create a new array with everything in the old array with the new data
+  };
+
+  const deleteTodo = () => {};
+  return (
+    <>
+      <h1>To dos:</h1>
+      <input onInput={onInput} />
+      <button onClick={addToDo}>Add</button>
+      {
+        //injecting javaScript
+        todos.map((todo) => (
+          <p>
+            <input type="checkbox" />
+            {todo}
+          </p>
+        )) //transform the items with map
+      }
+    </>
+  );
+};
